32b03cd9b3c289d659bc7cb1aa35fdeb8adca568
Joshua Britz <joshua@onepointfour.io>
2019-02-23 12:22:23 +0200

    Finshed Code Setup

--- script.js
+++ script.js
@@ -1,20 +1,79 @@
 let model;
 let webcamElement;
 let descriptionElement;
+let captureButton;
 let loaderElement;
+
+let predictionThreshold = 0.3;
+
+let showingresult = false;
 
 window.onload = async () => {
   webcamElement = document.querySelector('#webcam');
   descriptionElement = document.querySelector('#description');
+  captureButton = document.querySelector('#capture');
   loaderElement = document.querySelector('#loader');
 
   await setupWebcam(webcamElement, isMobileDevice());
 
   model = await mobilenet.load();
 
-  console.log('will remove loader');
+  descriptionElement.textContent = 'Scan an item';
+
   loaderElement.remove();
+
+  captureButton.addEventListener('click', classifyImage.bind(this));
 };
+
+async function classifyImage() {
+  if (!showingresult) {
+    showingresult = true;
+    setupVideoForClassification();
+
+    const predictions = await model.classify(webcamElement);
+
+    console.log(predictions);
+
+    if (predictions[0].probability > predictionThreshold) {
+      descriptionElement.textContent = `${getCleanClassName(
+        predictions[0].className
+      )} (${getCleanPercentage(predictions[0].probability)} sure)`;
+    } else {
+      descriptionElement.textContent = 'Not sure';
+    }
+
+    captureButton.textContent = 'Scan Something Else';
+  } else cleanUpVideoAfterClassification();
+}
+
+function setupVideoForClassification() {
+  webcamElement.width = webcamElement.clientWidth;
+  webcamElement.height = webcamElement.clientHeight;
+  webcamElement.pause();
+}
+
+function cleanUpVideoAfterClassification() {
+  descriptionElement.textContent = 'Scan an item';
+  captureButton.textContent = 'PREDICT';
+  showingresult = false;
+  webcamElement.play();
+}
+
+/**
+ *
+ * @param {Number} value
+ */
+function getCleanPercentage(value) {
+  return (value * 100).toFixed(2) + '%';
+}
+
+/**
+ *
+ * @param {String} className
+ */
+function getCleanClassName(className) {
+  return className.split(',')[0].trim();
+}
 
 /**
  *
